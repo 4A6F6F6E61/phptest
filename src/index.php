8c44cdefcaaf441bce6efcab7db5078e81c9a8a1
Zockedidock <32982128+Zockedidock@users.noreply.github.com>
2021-09-14 01:20:32 +0200

    login

--- src/index.php
+++ src/index.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <?php include "form.html"?>
-    <br>
     <?php
-        $firstname = $_GET["firstname"] ?? "";
-        $age = $_GET["age"] ?? "";
-        if($firstname != "" && $age != "")
-            echo "name: " . $firstname . "<br> Age: " .  $age;
+        $username = $_POST['username'] ?? "";
+        $password = $_POST['password'] ?? "";
+        
+        if ($username != "admin" && $password != "test")
+            include "login.html";
+        else
+            include "main.php";
     ?>
 </body>
 </html>
