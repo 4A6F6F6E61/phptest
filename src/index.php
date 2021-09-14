a1bfecc69dc283c9d882b1ea68f2dd0f235460d3
Joona Brückner <joona.brueckner@gmail.com>
2021-09-14 16:48:28 +0200

    all php now

--- src/index.php
+++ src/index.php
@@ -5,6 +5,14 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script>
+        var lang = navigator.language || navigator.userLanguage
+        console.log(lang)
+        if (lang == "en_US")
+            document.getElementsByTagName("html")[0].lang = "en"
+        else if (lang == "de")
+            document.getElementsByTagName("html")[0].lang = "de"
+    </script>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
@@ -12,10 +20,12 @@
         $username = $_POST['username'] ?? "";
         $password = $_POST['password'] ?? "";
         
-        if ($username != "admin" && $password != "test")
-            include "login.html";
-        else
+        if ($username === "admin" && $password === "test")
             include "main.php";
+        else if ($username != "" && $password != "") {
+            include "login.php";
+            echo "<script>alert(\"Wrong Password!\")</script>";
+        } else include "login.php";
     ?>
 </body>
 </html>
--- src/login.php
+++ src/login.php
@@ -1,11 +1,13 @@
 <login>
     <form action="index.php" method="post" id="login">
         <div class="form-group">
-            <label for="username">Username</label>
+            <label for="username" lang="en">Username</label>
+            <label for="username" lang="de">Benutzername</label>
             <input type="text" name="username" class="form-control" id="username" placeholder="Username">
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password" lang="en">Password</label>
+            <label for="password" lang="de">Passwort</label>
             <input type="password" name="password" class="form-control" id="password" placeholder="Password">
         </div>
         <br>
