4538506e89feef532d5669b1c72df69369b73c8e
Zockedidock <32982128+Zockedidock@users.noreply.github.com>
2021-09-19 01:44:46 +0200

    - fixed lang and bgc

--- src/home/sidebar.php
+++ src/home/sidebar.php
@@ -1,19 +1,23 @@
-<div class="sidebar">
+<div class="sidebar" lang="en">
     <div class="btn-group-vertical buttons">
         <a class="btn btn-secondary home-btn" href="./" lang="en">
             Home
         </a>
-        <a class="btn btn-secondary home-btn" href="./" lang="de">
-            Startseite
-        </a>
         <a class="btn btn-secondary logout-btn" href="../login/logout.php" lang="en">
             Logout
         </a>
-        <a class="btn btn-secondary logout-btn" href="../login/logout.php" lang="de">
-            Abmelden
-        </a>
         <a class="btn btn-secondary" lang="en">
             Settings
+        </a>
+    </div>
+</div>
+<div class="sidebar" lang="de">
+    <div class="btn-group-vertical buttons">
+        <a class="btn btn-secondary home-btn" href="./" lang="de">
+            Startseite
+        </a>
+        <a class="btn btn-secondary logout-btn" href="../login/logout.php" lang="de">
+            Abmelden
         </a>
         <a class="btn btn-secondary" lang="de">
             Einstellungen
