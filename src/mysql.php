b8f7df30c0c7ab7be9b831ddc68c32dd1a15d03b
Zockedidock <32982128+Zockedidock@users.noreply.github.com>
2021-09-27 19:26:47 +0200

    .

--- src/mysql.php
+++ src/mysql.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $name = 'social media app';
 $user = 'root';
-$password = 'ejYrkGnzPqZM';
+$password = 'J3UsnjXtA2XxYV';
 
 try {
     $mysql = new PDO("mysql:host=$host;dbname=$name", $user, $password);
